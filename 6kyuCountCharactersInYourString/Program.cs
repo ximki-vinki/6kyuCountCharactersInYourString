5a7ec17c5ad0c44f54fa4a18f1d73bfbab077ab9
Илья Манютин <chem-ixtrems@mail.ru>
2022-09-10 21:51:38 +0300

    Добавьте файлы проекта.

---
+++ 6kyuCountCharactersInYourString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6kyuCountCharactersInYourString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aavvs";
+
+            var answer = Kata.Count(str);
+        }
+    }
+ public class Kata
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            return str.GroupBy(x => x)
+                        .ToDictionary(x => x.Key, x => x.Count());
+        }
+    }
+}
